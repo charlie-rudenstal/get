5eb618d7437a6bd658d2fce1e03e09ebbd9391ad
Charlie Rudenstål <charlie4@gmail.com>
2012-09-26 12:26:25 +0200

    Added then method

--- sources/foo/index.js
+++ sources/foo/index.js
@@ -2,10 +2,9 @@ var query = require('../../lib/helpers/query');
 var json = require('../../lib/helpers/json');
 
 var foo = function(search, callback) {
-    callback(
-        json('http://services.tv.nu/search?query=hem&aggregate=program')
-        .get("results")
-    );
+    json('http://services.tv.nu/search?query=hem&aggregate=program')
+    .get("results")
+    .then(callback);
 }
 
 module.exports = {
