d2b4de0fb40e50daa94ed906d7422c071fcdc2d7
Charlie Rudenstål <charlie4@gmail.com>
2012-09-27 09:05:13 +0200

    FIX

--- sources/tvnu/index.js
+++ sources/tvnu/index.js
@@ -1,6 +1,7 @@
 var query = require('../../lib/helpers/query');
 
 var search = function(search, data, callback) { 
+    console.log("search", search);
     query
     .json('http://services.tv.nu/search?query='+escape(search)+'&aggregate=program')
     .get("results")
